31bae260fbb12a7a37d13e356a1ea0c66d45eb9f
varsey <eugeny.varseev@gmail.com>
2022-08-01 08:55:46 +0300

    feat - increment added

--- server.go
+++ server.go
@@ -1,27 +1,38 @@
 package main
 
 import (
-    "fmt"
-    "html"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
 )
 
-func main() {
+var counter int
+var mutex = &sync.Mutex{}
 
-    fmt.Println("Starting server...")
-
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-        fmt.Println("Hello")
-    })
-
-    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hi")
-        fmt.Println("Hi")
-    })
-
-    log.Fatal(http.ListenAndServe(":8081", nil))
-
+func echoString(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello")
+	fmt.Println("Hello")
 }
 
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(w, strconv.Itoa(counter))
+	mutex.Unlock()
+}
+
+func main() {
+	http.HandleFunc("/", echoString)
+
+	http.HandleFunc("/increment", incrementCounter)
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+		fmt.Println("Hi")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
