7190431ff0eedf174828cf1ca7ac10257452abaa
varsey <eugeny.varseev@gmail.com>
2022-08-01 09:03:26 +0300

    feat html page rendering

--- server.go
+++ server.go
@@ -4,33 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
-	"sync"
 )
 
-var counter int
-var mutex = &sync.Mutex{}
-
-func echoString(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello")
-	fmt.Println("Hello")
-}
-
-func incrementCounter(w http.ResponseWriter, r *http.Request) {
-	mutex.Lock()
-	counter++
-	fmt.Fprintf(w, strconv.Itoa(counter))
-	mutex.Unlock()
-}
-
 func main() {
-	http.HandleFunc("/", echoString)
 
-	http.HandleFunc("/increment", incrementCounter)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
 
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi")
-		fmt.Println("Hi")
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
